3849959584d11e8f8602158c8eb5f846183a081d
BoomManPro <wangqimeng@mininglamp.com>
2019-05-22 13:28:45 +0800

    添加actuator及项目关闭监听

---
+++ junit-project/src/main/java/cn/boommanpro/ApplicationCloseListener.java
@@ -0,0 +1,22 @@
+package cn.boommanpro;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ApplicationCloseListener implements ApplicationListener<ContextClosedEvent> {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        log.error("unit-test服务关闭");
+    }
+}
--- junit-project/src/main/java/cn/boommanpro/JunitProjectApplication.java
+++ junit-project/src/main/java/cn/boommanpro/JunitProjectApplication.java
@@ -4,7 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * @author BoomManPro
@@ -14,7 +20,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class JunitProjectApplication {
     public static void main(String[] args) {
-        new SpringApplication(JunitProjectApplication.class).run(args);
+        ConfigurableApplicationContext context = new SpringApplication(JunitProjectApplication.class).run();
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            TomcatServletWebServerFactory tomcatServletWebServerFactory = (TomcatServletWebServerFactory) context.getBean("tomcatServletWebServerFactory");
+            int port = tomcatServletWebServerFactory.getPort();
+            String contextPath = tomcatServletWebServerFactory.getContextPath();
+            log.info("<------------------------------------------ http://{}:{}{}/ ------------------------------------------>", host, port, contextPath);
+        } catch (UnknownHostException e) {
+            log.error("项目启动异常", e);
+        }
+        log.info("{}系统启动成功", JunitProjectApplication.class.getSimpleName());
     }
 
 
